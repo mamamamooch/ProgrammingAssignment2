771253fc9afe8fd714b0b70c0121184e7567b188
mamamamooch <mgcasople@mymail.mapua.edu.ph>
2021-09-28 21:29:22 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script makes use of makeCacheMatrix and cacheSolve functions
+## which are used for caching potentially time-consuming computations--
+## such as long vectors requiring the mean of the matrix to solve inverse.
 
-## Write a short comment describing this function
+## makeCacheMatrix consists of set, get,  setInverse, and getInverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() {x}
+  setInverse <- function(inverse) {inv <<- inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is used to get the cache data
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("retrieving cached data")
+    return(inv) ## Returns a matrix that is the inverse of 'x'
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
